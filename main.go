a7e325b876eab27ae39e5972912e773be50a6db2
chrisjsimpson <chris@karmacomputing.co.uk>
2021-07-17 18:15:08 +0200

    bind to 80, hello world

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+    "log"
+    "net/http"
     "fmt"
+    "html"
 )
 
-func main() {
-    fmt.Println("Hello world")
+func index(w http.ResponseWriter, r *http.Request) {
+    log.Println("Showing index")
+    fmt.Fprintf(w, fmt.Sprintf("Hello. you requested %s", html.EscapeString(r.URL.Path)))
 }
+
+func main() {
+    log.Println("Booting...")
+    http.HandleFunc("/", index)
+    log.Fatal(http.ListenAndServe(":80", nil))
+}
+
 
